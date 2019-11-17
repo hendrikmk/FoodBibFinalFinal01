faab3092879d2f7c606b3a8885a72fc28cd90953
Fleflux <felix@poehnitzsch.de>
2019-11-17 11:33:28 +0100

    alter DatabaseConnector deaktiviert

--- src/database/DatabaseConnector.js
+++ src/database/DatabaseConnector.js
@@ -1,57 +1,57 @@
-
-class DatabaseConnector {
-
-  constructor(){
-
-  }
-
-  defaultKey(){
-    return "myData";
-  }
-
-  write(key, json) {
-    window.localStorage.setItem(key, JSON.stringify(json));
-  }
-
-  read(key){
-    return JSON.parse(window.localStorage.getItem(key));
-  }
-
-    initMyDatabase(){
-    let data = [
-      {
-        'id': 0,
-        'rezeptname': 'Pfannkuchen',
-        'zutaten': '500g Mehl, 3 Eier, Salz',
-        'beschreibung': 'Alles Mischen und Ausbacken'
-      },
-      {
-        'id': 1,
-        'rezeptname': 'Fleischküchle',
-        'zutaten': 'Hackfleisch, Ei, Semmelbrösel',
-        'beschreibung': 'Alles mischen und in einer Pfanne gut durchbraten'
-      },
-      {
-        'id': 2,
-        'rezeptname': 'Grießbrei',
-        'zutaten': 'Milch, Weichweizengrieß, Zucker, Vanillezucker, Salz, Butter',
-        'beschreibung': 'Milch und Zucker zum kochen bringen. Weichweizengrieß mit einrühren und aufkochen lassen. Butter und Salz dazugeben. FERTIG!!'
-      },
-      {
-        'id': 3,
-        'rezeptname': 'Haxe mit Kraut',
-        'zutaten': 'Haxe, Rotkraut, Knödel',
-        'beschreibung': 'Alles in der guten alten Mikki warmmachen und genießen. Fertig!'
-      }
-    ];
-
-    const key = this.defaultKey();
-    window.localStorage.setItem(key, JSON.stringify(data));
-  }
-
-  isDataObjectAlreadyPersisted(){
-    return window.localStorage.getItem(this.defaultKey) != 'undefined';
-  }
-}
-
-export default DatabaseConnector;
+//
+// class DatabaseConnector {
+//
+//   constructor(){
+//
+//   }
+//
+//   defaultKey(){
+//     return "myData";
+//   }
+//
+//   write(key, json) {
+//     window.localStorage.setItem(key, JSON.stringify(json));
+//   }
+// 
+//   read(key){
+//     return JSON.parse(window.localStorage.getItem(key));
+//   }
+//
+//     initMyDatabase(){
+//     let data = [
+//       {
+//         'id': 0,
+//         'rezeptname': 'Pfannkuchen',
+//         'zutaten': '500g Mehl, 3 Eier, Salz',
+//         'beschreibung': 'Alles Mischen und Ausbacken'
+//       },
+//       {
+//         'id': 1,
+//         'rezeptname': 'Fleischküchle',
+//         'zutaten': 'Hackfleisch, Ei, Semmelbrösel',
+//         'beschreibung': 'Alles mischen und in einer Pfanne gut durchbraten'
+//       },
+//       {
+//         'id': 2,
+//         'rezeptname': 'Grießbrei',
+//         'zutaten': 'Milch, Weichweizengrieß, Zucker, Vanillezucker, Salz, Butter',
+//         'beschreibung': 'Milch und Zucker zum kochen bringen. Weichweizengrieß mit einrühren und aufkochen lassen. Butter und Salz dazugeben. FERTIG!!'
+//       },
+//       {
+//         'id': 3,
+//         'rezeptname': 'Haxe mit Kraut',
+//         'zutaten': 'Haxe, Rotkraut, Knödel',
+//         'beschreibung': 'Alles in der guten alten Mikki warmmachen und genießen. Fertig!'
+//       }
+//     ];
+//
+//     const key = this.defaultKey();
+//     window.localStorage.setItem(key, JSON.stringify(data));
+//   }
+//
+//   isDataObjectAlreadyPersisted(){
+//     return window.localStorage.getItem(this.defaultKey) != 'undefined';
+//   }
+// }
+//
+// export default DatabaseConnector;
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ window.addEventListener("load", () => {
     let app = new App();
     var database = firebase.database();
     var storage = firebase.storage();
-    let db = new DatabaseConnector();
+//    let db = new DatabaseConnector();
     if(!db.isDataObjectAlreadyPersisted()){
         db.initMyDatabase();
     }
